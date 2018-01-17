58abde195b22ca6c27ea173933cd9ae8330a1b87
Shannon Prescott <shan.prescott@outlook.com>
2018-01-17 20:02:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,3 +24,15 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array
 }
 
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length-1)
+}
