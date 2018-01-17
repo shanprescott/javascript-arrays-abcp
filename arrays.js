305a8d3fba0f9f2ac2bcafee2dac9ef82feb228a
Shannon Prescott <shan.prescott@outlook.com>
2018-01-17 18:56:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,4 +16,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 }
-function accessElementinArray
+function accessElementinArray(array, index) {
+  
+}
